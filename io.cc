9dc984ab3a4cdf07881358859e1529b58904dd3a
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-09-17 20:51:54 -0500

    simple i/o

---
+++ io.cc
@@ -0,0 +1,21 @@
+/* Task 
+Read  numbers from stdin and print their sum to stdout.*/
+
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    int n = a + b + c;
+    cout << n << endl;
+        
+    return 0;
+}
