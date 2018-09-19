0e92fd466069faee0b7f495c52b27bfd901de151
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-09-18 22:03:36 -0500

    pointer abs

---
+++ Data Structures/arrays.cc
@@ -0,0 +1,16 @@
+/* Given an array, , of  integers, print each element in reverse 
+order as a single line of space-separated integers.
+*/
+
+using namespace std;
+
+
+// Complete the reverseArray function below.
+vector<int> reverseArray(vector<int> a) {
+	vector<int> reversed;
+	for(int i = a.size(); i <=0; i--){
+		reversed.push_back(i);
+	}
+	cout << endl;
+
+}
