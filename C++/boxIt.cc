a77f4fffe5b2db7927f29d55d2986a77d922d659
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-09-26 21:16:44 -0500

    classes box it

---
+++ C++/boxIt.cc
@@ -0,0 +1,19 @@
+/*
+Design a class named Box whose dimensions are integers and private to the class. 
+The dimensions are labelled: length l, breadth b, and height h.
+
+The default constructor of the class should initialize l, b, and h to 0.
+
+The parameterized constructor Box(int length, int breadth, int height) should 
+initialize Box's l, b and h to length, breadth and height.
+
+The copy constructor BoxBox ) should set l, b and h to B's l, b, and h, respectively.
+
+Apart from the above, the class should have 4 functions:
+
+int getLength() - Return box's length
+int getBreadth() - Return box's breadth
+int getHeight() - Return box's height
+long long CalculateVolume() - Return the volume of the box
+
+*/
