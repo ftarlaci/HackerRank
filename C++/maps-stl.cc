4033dc255e3acdbd58f07bbf03b0c6c56afcba29
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-09-27 22:32:16 -0500

    maps STL

---
+++ C++/maps-stl.cc
@@ -0,0 +1,12 @@
+/*
+You are appointed as the assistant to a teacher in a school and she is correcting 
+the answer sheets of the students.Each student can have multiple answer sheets.So 
+the teacher has  queries:
+
+ 1 X Y :Add the marks  to the student whose name is .
+
+2 X: Erase the marks of the students whose name is .
+
+3 X : Print the marks of the students whose name is . (If  didn't get any marks print .)
+
+*/
