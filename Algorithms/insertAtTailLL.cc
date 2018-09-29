592636979053735f4337f199b1ffe3c2d0824e41
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-09-28 22:17:31 -0500

    insert at Tail LL

---
+++ Algorithms/insertAtTailLL.cc
@@ -0,0 +1,22 @@
+/*
+You are given the pointer to the head node of a linked 
+list and an integer to add to the list. Create a new node 
+with the given integer. Insert this node at the tail of the 
+linked list and return the head node of the linked list formed 
+after inserting this new node. The given head pointer may be null, 
+meaning that the initial list is empty.
+*/
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* insertNodeAtTail(SinglyLinkedListNode* head, int data) {
+
+
+}
