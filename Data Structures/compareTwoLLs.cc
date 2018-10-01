545378df47b22aba83546ac2929f3898e7aead6e
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-10-01 11:09:22 -0500

    are the two LLs identical?

---
+++ Data Structures/compareTwoLLs.cc
@@ -0,0 +1,22 @@
+/*
+You’re given the pointer to the head nodes of two linked lists. 
+Compare the data in the nodes of the linked lists to check if 
+they are equal. The lists are equal only if they have the same 
+number of nodes and corresponding nodes contain the same data. 
+Either head pointer given may be null meaning that the corresponding 
+list is empty.
+*/
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+bool compare_lists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+
+
+}
