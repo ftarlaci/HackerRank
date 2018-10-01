5780b6d1f6c1f1360dec57355eeb5102a3e72a9f
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-10-01 10:30:20 -0500

    delete a node LL

---
+++ Data Structures/deleteANode.cc
@@ -0,0 +1,24 @@
+/* You’re given the pointer to the head node of a linked 
+list and the position of a node to delete. Delete the node 
+at the given position and return the head node. A position 
+of 0 indicates head, a position of 1 indicates one node away 
+from the head and so on. The list may become empty after you 
+delete the node.
+*/
+
+
+
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* head, int position) {
+
+
+}
